11c73f9144ef4ae653fc0c87f4df40ea50d03185
kok-s0s <2694308562@qq.com>
2022-03-09 23:28:05 +0800

    set Display Sizes

--- electron/main.ts
+++ electron/main.ts
@@ -11,51 +11,53 @@ declare const MAIN_WINDOW_PRELOAD_WEBPACK_ENTRY: string
 //     : app.getAppPath()
 
 function createWindow() {
-  mainWindow = new BrowserWindow({
-    icon: "assets/kok-s0s.icns",
-    width: 1300,
-    height: 800,
-    minWidth: 1300,
-    minHeight: 800,
-    backgroundColor: '#191622',
-    webPreferences: {
-      nodeIntegration: false,
-      contextIsolation: true,
-      preload: MAIN_WINDOW_PRELOAD_WEBPACK_ENTRY
-    }
-  })
+    mainWindow = new BrowserWindow({
+        icon: 'assets/kok-s0s.icns',
+        width: 1300,
+        height: 800,
+        minWidth: 1300,
+        minHeight: 800,
+        maxWidth: 1400,
+        maxHeight: 820,
+        backgroundColor: '#191622',
+        webPreferences: {
+            nodeIntegration: false,
+            contextIsolation: true,
+            preload: MAIN_WINDOW_PRELOAD_WEBPACK_ENTRY
+        }
+    })
 
-  mainWindow.loadURL(MAIN_WINDOW_WEBPACK_ENTRY)
+    mainWindow.loadURL(MAIN_WINDOW_WEBPACK_ENTRY)
 
-  mainWindow.webContents.openDevTools()
+    mainWindow.webContents.openDevTools()
 
-  mainWindow.on('closed', () => {
-    mainWindow = null
-  })
+    mainWindow.on('closed', () => {
+        mainWindow = null
+    })
 }
 
 async function registerListeners() {
-  /**
-   * This comes from bridge integration, check bridge.ts
-   */
-  ipcMain.on('message', (_, message) => {
-    console.log(message)
-  })
+    /**
+     * This comes from bridge integration, check bridge.ts
+     */
+    ipcMain.on('message', (_, message) => {
+        console.log(message)
+    })
 }
 
 app.on('ready', createWindow)
-  .whenReady()
-  .then(registerListeners)
-  .catch(e => console.error(e))
+    .whenReady()
+    .then(registerListeners)
+    .catch(e => console.error(e))
 
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
 })
 
 app.on('activate', () => {
-  if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow()
-  }
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow()
+    }
 })
