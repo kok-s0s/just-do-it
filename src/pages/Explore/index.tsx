bfab1d4740f75800bda77d7f715a417ed2053968
kok-s0s <2694308562@qq.com>
2022-03-10 22:29:29 +0800

    add: Explore Page

--- src/pages/Explore/index.tsx
+++ src/pages/Explore/index.tsx
@@ -1,43 +1,47 @@
-import { useState, SyntheticEvent } from 'react';
-import Box from '@mui/material/Box';
-import Tab from '@mui/material/Tab';
-import TabContext from '@mui/lab/TabContext';
-import TabList from '@mui/lab/TabList';
-import TabPanel from '@mui/lab/TabPanel';
+import { useState, SyntheticEvent } from 'react'
+import Box from '@mui/material/Box'
+import Tab from '@mui/material/Tab'
+import TabContext from '@mui/lab/TabContext'
+import TabList from '@mui/lab/TabList'
+import TabPanel from '@mui/lab/TabPanel'
 import { Container, Back, BText } from './styles'
 import { Link } from 'react-router-dom'
 
 export function Explore() {
-  const [value, setValue] = useState('1');
+    const [value, setValue] = useState('1')
 
-  const handleChange = (event: SyntheticEvent, newValue: string) => {
-    setValue(newValue);
-  };
+    const handleChange = (event: SyntheticEvent, newValue: string) => {
+        setValue(newValue)
+    }
 
-
-  return (
-    <Container>
-      <Box sx={{ width: '100%', typography: 'body1' }}>
-        <TabContext value={value}>
-          <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-            <TabList onChange={handleChange} aria-label="lab API tabs example">
-              <Tab label="Item One" value="1" />
-              <Tab label="Item Two" value="2" />
-              <Tab label="Item Three" value="3" />
-            </TabList>
-          </Box>
-          <TabPanel value="1">Item One</TabPanel>
-          <TabPanel value="2">Item Two</TabPanel>
-          <TabPanel value="3">Item Three</TabPanel>
-        </TabContext>
-      </Box>
-
-      <Link to="/home">
-        <Back>
-          <BText>返回</BText>
-        </Back>
-      </Link>
-    </Container>
-  )
+    return (
+        <>
+            <Container>
+                <Box sx={{ width: '100%', typography: 'body1' }}>
+                    <TabContext value={value}>
+                        <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+                            <TabList
+                                onChange={handleChange}
+                                aria-label="lab API tabs example"
+                            >
+                                <Tab label="技术前沿" value="1" />
+                                <Tab label="书籍推荐" value="2" />
+                                <Tab label="项目推荐" value="3" />
+                                <Tab label="码无止境" value="4" />
+                            </TabList>
+                        </Box>
+                        <TabPanel value="1">Item One</TabPanel>
+                        <TabPanel value="2">Item Two</TabPanel>
+                        <TabPanel value="3">Item Three</TabPanel>
+                        <TabPanel value="4">Item Four</TabPanel>
+                    </TabContext>
+                </Box>
+            </Container>
+            <Link to="/home">
+                <Back>
+                    <BText>返回</BText>
+                </Back>
+            </Link>
+        </>
+    )
 }
-
--- src/pages/Explore/styles.ts
+++ src/pages/Explore/styles.ts
@@ -1,39 +1,35 @@
 import styled from 'styled-components'
 
 export const Container = styled.div`
-  height: 100vh;
-  // display: flex;
-  // flex-direction: column;
-  // align-items: center;
-  // justify-content: center;
-  background-color: #D8EFEF;
+    height: 100vh;
+    background-color: #d8efef;
 `
 
 export const Back = styled.button`
-  position: fixed;
-  right: 1.5rem;
-  bottom: 1.5rem;
-  border-radius: 50%;
-  padding: 15px;
-  background-color: #69A9C9;
-  color: white;
-  cursor: pointer;
-  border: none;
-  font-size: 1rem;
-  &:hover,
-  &:focus {
-    color: #3874CB;
-  }
-  &:active {
-    color: #cfdef2;
-  }
+    position: fixed;
+    right: 1.5rem;
+    bottom: 1.5rem;
+    border-radius: 50%;
+    padding: 15px;
+    background-color: #69a9c9;
+    color: white;
+    cursor: pointer;
+    border: none;
+    font-size: 1rem;
+    &:hover,
+    &:focus {
+        color: #3874cb;
+    }
+    &:active {
+        color: #cfdef2;
+    }
 `
 
 export const BText = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center; 
-  box-sizing: border-box;
-  width: 32px;
-  height: 32px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    box-sizing: border-box;
+    width: 32px;
+    height: 32px;
 `
