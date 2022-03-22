abfbe2df2c15af747442ed22b6d9d661dcfc9ca0
kok-s0s <2694308562@qq.com>
2022-03-23 00:45:18 +0800

    add proURL

--- src/api/axios.config.ts
+++ src/api/axios.config.ts
@@ -1,7 +1,11 @@
 import axios from 'axios'
 
+const curState = 'pro'
+const devURL = 'http://localhost:27017/'
+const proURL = 'http://106.52.175.59:27017/'
+
 const instance = axios.create({
-    baseURL: 'http://localhost:27017/',
+    baseURL: curState === 'pro' ? proURL : devURL,
     headers: {
         'Content-Type': 'application/json'
     }
