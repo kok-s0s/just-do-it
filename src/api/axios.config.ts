e3d6f0eec08a6b957905d8b32f985ad19ae26f5d
kok-s0s <2694308562@qq.com>
2022-03-28 00:36:10 +0800

    update axios.config

--- src/api/axios.config.ts
+++ src/api/axios.config.ts
@@ -1,11 +1,10 @@
 import axios from 'axios'
 
-const curState = 'pro'
-const devURL = 'http://localhost:27017/'
+// const devURL = 'http://localhost:27017/'
 const proURL = 'http://106.52.175.59:27017/'
 
 const instance = axios.create({
-    baseURL: curState === 'pro' ? proURL : devURL,
+    baseURL: proURL,
     headers: {
         'Content-Type': 'application/json'
     }
