194cc75c2ab9a7bb212acc2b54153d00b1649953
kok-s0s <2694308562@qq.com>
2022-03-29 23:06:48 +0800

    add MySelf component

---
+++ src/components/MySelf/index.tsx
@@ -0,0 +1,91 @@
+import { ArtBoard } from './styles'
+import { Graph } from '@antv/x6'
+import { Component } from 'react'
+
+const data = {
+    nodes: [
+        {
+            id: 'node1',
+            x: 40,
+            y: 40,
+            width: 100,
+            height: 40,
+            attrs: {
+                body: {
+                    fill: '#2ECC71',
+                    stroke: '#000',
+                    strokeDasharray: '10,2'
+                },
+                label: {
+                    text: 'Hello',
+                    fill: '#333',
+                    fontSize: 13
+                }
+            }
+        },
+        {
+            id: 'node2',
+            x: 180,
+            y: 240,
+            width: 100,
+            height: 40,
+            attrs: {
+                body: {
+                    fill: '#F39C12',
+                    stroke: '#000',
+                    rx: 16,
+                    ry: 16
+                },
+                label: {
+                    text: 'World',
+                    fill: '#333',
+                    fontSize: 18,
+                    fontWeight: 'bold',
+                    fontVariant: 'small-caps'
+                }
+            }
+        }
+    ],
+    edges: [
+        {
+            source: 'node1',
+            target: 'node2'
+        }
+    ]
+}
+
+export default class MySelf extends Component {
+    // @ts-ignore
+    private container: HTMLDivElement
+
+    componentDidMount() {
+        const graph = new Graph({
+            container: this.container,
+            scroller: {
+                enabled: true,
+                pannable: true,
+                pageVisible: true,
+                pageBreak: false
+            },
+            mousewheel: {
+                enabled: true,
+                modifiers: ['ctrl', 'meta']
+            }
+        })
+
+        graph.fromJSON(data)
+
+        graph.drawBackground({
+            color: '#f5f5f5'
+        })
+        graph.centerContent()
+    }
+
+    refContainer = (container: HTMLDivElement) => {
+        this.container = container
+    }
+
+    render() {
+        return <ArtBoard ref={this.refContainer} />
+    }
+}
---
+++ src/components/MySelf/styles.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+
+export const ArtBoard = styled.div`
+    height: 100%;
+    width: 50%;
+    margin-left: 8px;
+    margin-right: 8px;
+    box-shadow: 0 0 10px 1px #e9e9e9;
+`
