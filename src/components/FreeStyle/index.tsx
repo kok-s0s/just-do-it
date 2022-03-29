9236a71cb650338000adb6a06d1a51d58543d9e5
kok-s0s <2694308562@qq.com>
2022-03-29 23:06:32 +0800

    add FreeStyle component

---
+++ src/components/FreeStyle/index.tsx
@@ -0,0 +1,10 @@
+import { Container } from './styles'
+import MySelf from '../MySelf'
+
+export function FreeStyle() {
+    return (
+        <Container>
+            <MySelf />
+        </Container>
+    )
+}
