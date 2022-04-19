05dde675ae5694185754aaa65da514f871e1b383
kok-s0s <2694308562@qq.com>
2022-04-20 00:51:30 +0800

    delete everything about X6

--- src/components/FreeStyle/index.tsx
+++ src/components/FreeStyle/index.tsx
@@ -1,10 +1,5 @@
 import { Container } from './styles'
-import MySelf from '../MySelf'
 
 export function FreeStyle() {
-    return (
-        <Container>
-            <MySelf />
-        </Container>
-    )
+    return <Container></Container>
 }
