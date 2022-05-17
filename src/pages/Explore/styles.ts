a14c4800d6705e8800b91015ea89396734517228
kok-s0s <2694308562@qq.com>
2022-05-18 02:38:45 +0800

    feat: update Explore page

--- src/pages/Explore/styles.ts
+++ src/pages/Explore/styles.ts
@@ -110,18 +110,3 @@ export const Next = styled.a`
         color: #000;
     }
 `
-
-export const SideBar = styled.div`
-    position: fixed;
-    top: 0;
-    right: 0;
-    display: flex;
-    justify-content: center;
-    width: 5vw;
-    padding: 1rem 0.2rem;
-`
-
-export const Back = styled(motion.div)`
-    font-size: 3rem;
-    color: black;
-`
