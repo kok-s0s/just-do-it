e371977929355d9b222057da5c6830ddcd3dc5ba
kok-s0s <2694308562@qq.com>
2022-05-18 03:01:22 +0800

    feat: update ControlPanel layout

--- src/layouts/ControlPanel/styles.ts
+++ src/layouts/ControlPanel/styles.ts
@@ -6,9 +6,28 @@ export const SideBar = styled.div`
     top: 0;
     right: 0;
     display: flex;
-    justify-content: center;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: center;
     width: 5vw;
+    height: 100vh;
     padding: 1rem 0.2rem;
+    background-color: #bae8e8;
+`
+
+export const Options = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: center;
+    height: 30vh;
+`
+
+export const Image = styled.img`
+    display: block;
+    width: 70%;
+    height: auto;
+    border-radius: 50%;
 `
 
 export const Back = styled(motion.div)`
