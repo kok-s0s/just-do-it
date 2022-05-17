99249d371d346b1e6fedba2033019ea47c5ff6bc
kok-s0s <2694308562@qq.com>
2022-05-18 01:54:26 +0800

    fix: component Link div bug

--- src/layouts/Basic/styles.ts
+++ src/layouts/Basic/styles.ts
@@ -1,9 +1,11 @@
+import { motion } from 'framer-motion'
 import styled from 'styled-components'
 
-export const Icon = styled.img`
+export const Icon = styled(motion.div)`
     position: fixed;
     right: 1.5rem;
-    bottom: 1.5rem;
-    width: 70px;
+    bottom: 0.7rem;
+    font-size: 4.2rem;
+    color: black;
     opacity: 0.4;
 `
