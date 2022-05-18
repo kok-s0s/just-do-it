1a3db8cb75b80c8a74e5ee6a8f9e35df444243c4
kok-s0s <2694308562@qq.com>
2022-05-18 14:17:54 +0800

    feat: add quote function

--- src/layouts/ControlPanel/styles.ts
+++ src/layouts/ControlPanel/styles.ts
@@ -27,11 +27,27 @@ export const Image = styled.img`
     display: block;
     width: 70%;
     height: auto;
+    margin: 1rem 0;
     border-radius: 50%;
 `
 
-export const Back = styled(motion.div)`
+export const Icon = styled(motion.div)`
+    display: flex;
     font-size: 3rem;
     color: black;
+    opacity: 0.7;
 `
 
+interface QuoteProps {
+  family: string
+  size: string
+}
+
+export const Quote = styled.div<QuoteProps>`
+    writing-mode: vertical-rl;
+    text-orientation: upright;
+    font-family: ${props => props.family};
+    font-size: ${props => props.size};
+    letter-spacing: 0.5rem;
+    opacity: 0.45;
+`
