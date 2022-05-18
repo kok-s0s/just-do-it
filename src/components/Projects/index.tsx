b0f479cae83ae6e8aed3ec1b52e221799da464bc
kok-s0s <2694308562@qq.com>
2022-05-18 11:22:26 +0800

    feat: set Projects as component

---
+++ src/components/Projects/index.tsx
@@ -0,0 +1,86 @@
+import Card from '@mui/material/Card'
+import CardContent from '@mui/material/CardContent'
+import CardMedia from '@mui/material/CardMedia'
+import Typography from '@mui/material/Typography'
+import { useEffect } from 'react'
+import { useLocalStorage } from '../../hooks/useLocalStorage'
+import { getProjects } from '../../api/practicalproject'
+import { ProjectList } from './styles'
+
+interface Project {
+  name: string
+  description: string
+  link: string
+  image: string
+}
+
+export function Projects() {
+  const [projects, setProjects] = useLocalStorage('projects', [])
+
+  useEffect(() => {
+    const getData = async () => {
+      const res = await getProjects()
+
+      if (res.status === 200)
+        setProjects(res.data)
+    }
+
+    getData()
+  }, [])
+
+  return (
+    <ProjectList>
+      {projects.map((project: Project, index: number) => {
+        return (
+          <Card
+              key={index}
+              color="#fffffe"
+              variant="outlined"
+              sx={{
+                'maxWidth': '45%',
+                'minHeight': '500px',
+                'margin': '1rem 0',
+                '&:first-child': {
+                  marginTop: 0,
+                },
+                '&:last-child': {
+                  marginBottom: 0,
+                },
+              }}
+            >
+            <CardContent sx={{ fontFamily: '\'Gill Sans\', \'Noto Serif SC\'' }}>
+              <Typography
+                  sx={{
+                    fontSize: 24,
+                    fontWeight: 500,
+                    letterSpacing: '0.1rem',
+                    opacity: '0.8',
+                  }}
+                  gutterBottom
+                >
+                {project.name}
+              </Typography>
+              <Typography
+                  sx={{
+                    fontSize: '1.1rem',
+                    fontFamily: '\'Gill Sans\', \'Noto Serif SC\'',
+                    letterSpacing: '0.3rem',
+                    opacity: '0.7',
+                  }}
+                >
+                {project.description}
+              </Typography>
+            </CardContent>
+            <CardMedia
+                component="img"
+                sx={{ width: '100%' }}
+                height="300"
+                image={project.image}
+                alt={project.name}
+              />
+          </Card>
+        )
+      })}
+    </ProjectList>
+  )
+}
--- src/components/Projects/styles.ts
+++ src/components/Projects/styles.ts
@@ -3,30 +3,12 @@ import styled from 'styled-components'
 export const ProjectList = styled.div`
     display: flex;
     flex-direction: column;
-    max-height: 85vh;
-    margin-left: 2rem;
+    max-height: 75vh;
     -ms-overflow-style: none; /* for Internet Explorer, Edge */
     scrollbar-width: none; /* for Firefox */
     scrollbar-width: none;
     overflow-y: scroll;
     &::-webkit-scrollbar {
         display: none;
-    }
-`
-
-export const Link = styled.a`
-    display: flex;
-    justify-content: center;
-    align-content: center;
-    float: right;
-    color: #8bb7b9;
-    &:focus,
-    &:active {
-        color: black;
-    }
-    &:before,
-    &after {
-        content: '点点看 =>';
-        font-size: 1rem;
     }
 `
