9fb7532fb42da19846bf4cddb58f737f49ccb935
kok-s0s <2694308562@qq.com>
2022-05-19 14:04:04 +0800

    fix: delete try-catch

--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 export const useLocalStorage = <T>(
   key: string,
@@ -7,34 +7,27 @@ export const useLocalStorage = <T>(
   // State to store our value
   // Pass initial state function to useState so logic is only executed once
   const [storedValue, setStoredValue] = useState<T>(() => {
-    try {
-      // Get from local storage by key
-      const item = window.localStorage.getItem(key)
-      // Parse stored json or if none return initialValue
-      return item ? JSON.parse(item) : initialValue
-    }
-    catch (error) {
-      // If error also return initialValue
-      return initialValue
-    }
+    // Get from local storage by key
+    const item = window.localStorage.getItem(key)
+    // Parse stored json or if none return initialValue
+    return item !== null ? JSON.parse(item) : initialValue
   })
 
   // Return a wrapped version of useState's setter function that ...
   // ... persists the new value to localStorage.
   const setValue = (value: T | ((val: T) => T)) => {
-    try {
-      // Allow value to be a function so we have same API as useState
-      const valueToStore
+    // Allow value to be a function so we have same API as useState
+    const valueToStore
         = value instanceof Function ? value(storedValue) : value
-      // Save state
-      setStoredValue(valueToStore)
-      // Save to local storage
-      window.localStorage.setItem(key, JSON.stringify(valueToStore))
-    }
-    catch (error) {
-      // A more advanced implementation would handle the error case
-    }
+    // Save state
+    setStoredValue(valueToStore)
+    // Save to local storage
+    window.localStorage.setItem(key, JSON.stringify(valueToStore))
   }
+
+  useEffect(() => {
+    window.localStorage.setItem(key, JSON.stringify(initialValue))
+  }, [])
 
   return [storedValue, setValue]
 }
