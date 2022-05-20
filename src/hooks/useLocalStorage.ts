4e29a5044986a0344734c392e35130cc761cf394
kok-s0s <2694308562@qq.com>
2022-05-20 13:40:13 +0800

    fix: disable set again

--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -1,33 +1,22 @@
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 
 export const useLocalStorage = <T>(
   key: string,
   initialValue: T,
 ): [T, (value: T | ((val: T) => T)) => void] => {
-  // State to store our value
-  // Pass initial state function to useState so logic is only executed once
   const [storedValue, setStoredValue] = useState<T>(() => {
-    // Get from local storage by key
-    const item = window.localStorage.getItem(key)
-    // Parse stored json or if none return initialValue
-    return item !== null ? JSON.parse(item) : initialValue
+    if (typeof window !== 'undefined') {
+      const saved = localStorage.getItem(key)
+      const initial = saved !== null ? JSON.parse(saved) : initialValue
+      return initial
+    }
   })
 
-  // Return a wrapped version of useState's setter function that ...
-  // ... persists the new value to localStorage.
   const setValue = (value: T | ((val: T) => T)) => {
-    // Allow value to be a function so we have same API as useState
-    const valueToStore
-        = value instanceof Function ? value(storedValue) : value
-    // Save state
+    const valueToStore = value instanceof Function ? value(storedValue) : value
     setStoredValue(valueToStore)
-    // Save to local storage
-    window.localStorage.setItem(key, JSON.stringify(valueToStore))
+    localStorage.setItem(key, JSON.stringify(valueToStore))
   }
-
-  useEffect(() => {
-    window.localStorage.setItem(key, JSON.stringify(initialValue))
-  }, [])
 
   return [storedValue, setValue]
 }
