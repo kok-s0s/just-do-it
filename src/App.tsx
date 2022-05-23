cf0fd712fbd6307ba768032a54ef85bf6422e466
kok-s0s <2694308562@qq.com>
2022-05-23 19:42:27 +0800

    feat: add Stack page

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ import { CodeBlock } from './pages/CodeBlock'
 import { Animes } from './pages/Animes'
 import { BookMark } from './pages/BookMark'
 import { Markdown } from './pages/Markdonw'
+import { Stack } from './pages/Stack'
 
 export function App() {
   return (
@@ -25,6 +26,7 @@ export function App() {
           <Route path="animes" element={<Animes />} />
           <Route path="bookmark" element={<BookMark />} />
           <Route path="markdown" element={<Markdown />} />
+          <Route path="stack" element={<Stack />} />
         </Routes>
       </HashRouter>
     </>
