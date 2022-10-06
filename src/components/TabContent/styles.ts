8462fad420ce50bba11f34e9424359dbe3e56a85
kok-s0s <2694308562@qq.com>
2022-10-06 17:07:08 +0800

    refactor: delete uselessful component

--- src/components/TabContent/styles.ts
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components'
-
-export const Container = styled.div`
-  margin-bottom: 2rem;
-  height: auto;
-  background: #fffffe;
-  padding: 1.5rem;
-  box-shadow: -10px 10px 0px 0px black;
-`
--- src/components/Tabs/styles.ts
+++
@@ -1,5 +0,0 @@
-import styled from 'styled-components'
-
-export const Options = styled.ul`
-  display: flex;
-`
